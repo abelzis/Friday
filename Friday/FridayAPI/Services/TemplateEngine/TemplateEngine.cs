e481cd200358d91a35db1ad0bd56614d500a9b48
abelzis <abelzis@hotmail.com>
2021-03-22 02:27:19 +0200

    template-engine-initial-add: added template generator controller, added basic template engine

---
+++ Friday/FridayAPI/Services/TemplateEngine/TemplateEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Xceed.Words.NET;
+
+namespace FridayAPI.Services.TemplateEngine
+{
+    class TemplateEngine
+    {
+        public void GenerateWordFile(object sourceTemplateName, object targetFileName)
+        {
+            if (File.Exists((string)sourceTemplateName))
+            {
+                var doc = DocX.Load(sourceTemplateName.ToString());
+                var newDoc = doc.Copy();
+
+                newDoc.ReplaceText("<text>", "Placeholder text");
+
+                try
+                {
+                    newDoc.SaveAs(targetFileName.ToString());
+                }
+                catch (Exception e)
+                {
+                    throw new IOException("Error: File could not be saved. " + e.ToString());
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("Error: Template file not found while trying to generate document.");
+            }
+        }
+    }
+}
--- Friday/FridayAPI/Startup.cs
+++ Friday/FridayAPI/Startup.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
