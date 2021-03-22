c72ed78f38194350bb564d8f39ad8ba85e2db116
abelzis <abelzis@hotmail.com>
2021-03-22 03:59:54 +0200

    basic-pages: added two basic pages, prettier update, added navigation

---
+++ Friday/friday-client/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class HomePage extends Component {
+    render() {
+        return <div className="mt-5 d-flex justify-content-left">Welcome to Friday</div>;
+    }
+}
